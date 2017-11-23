1cc87fcd6e5776187d0750f72886bdbf4ed0938d
jinwoo <cjw0672@naver.com>
2017-11-23 11:10:55 +0900

    update Private Key

--- fabric-scripts/hlfv1/generateArtifact.sh
+++ fabric-scripts/hlfv1/generateArtifact.sh
@@ -24,6 +24,19 @@ if [ "$?" -ne 0 ]; then
 fi
 mv crypto-config $DIR/composer
 
+
+## change PRIVATE KEY
+# Copy the template to the file that will be modified to add the private key
+cp $DIR/composer/docker-compose-template.yml $DIR/composer/docker-compose.yml
+
+CURRENT_DIR=$DIR/composer
+cd $CURRENT_DIR/crypto-config/peerOrganizations/org1.example.com/ca/
+PRIV_KEY=$(ls *_sk)
+cd "$CURRENT_DIR"
+sed -i "s/PRIVATE_KEY/${PRIV_KEY}/g" docker-compose.yml
+
+
+
 # generate genesis block for orderer
 ~/bin/configtxgen -profile ComposerOrdererGenesis -outputBlock $DIR/composer/composer-genesis.block
 if [ "$?" -ne 0 ]; then
--- fabric-scripts/hlfv1/teardownFabric.sh
+++ fabric-scripts/hlfv1/teardownFabric.sh
@@ -13,11 +13,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"/composer
 ARCH=$ARCH docker-compose -f docker-compose.yml kill && docker-compose -f docker-compose.yml down
 
+rm -rf docker-compose.yml
+
+
 # remove the local state
 #rm -rf ~/.composer-connection-profiles/hlfv1
 #rm -f ~/.composer-credentials/*
 
 # remove chaincode docker images
-# docker rmi $(docker images dev-* -q)
+image=$(docker images dev-* -q)
+if [ "$images" != "" ]; then
+ docker rmi -f $images
+fi
 
 # Your system is now clean
